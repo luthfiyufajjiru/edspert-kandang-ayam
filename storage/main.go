c733c28095a27ab7791b57f8ef3aa39c6f80f325
yufajjiru-dev <luthfi.yufajjiru@efishery.com>
2023-08-04 00:32:16 +0700

    feat(storage): add safe global inmem storage - Append - Len - Range

--- storage/main.go
+++ storage/main.go
@@ -1,1 +1,44 @@
 package storage
+
+import (
+	"strconv"
+	"sync"
+)
+
+type SerialFloat struct {
+	data []float64
+	mtx  sync.RWMutex
+}
+
+func (s *SerialFloat) Append(data string) {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	dataF, err := strconv.ParseFloat(data, 64)
+	if err == nil {
+		s.data = append(s.data, dataF)
+	}
+}
+
+func (s *SerialFloat) Len() (res int) {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	res = len(s.data)
+	return
+}
+
+func (s *SerialFloat) Range(start, end int) (res []float64) {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	ln := len(s.data)
+	if ln == 0 {
+		return
+	}
+	if start < 0 {
+		start = 0
+	}
+	if end > ln {
+		end = ln
+	}
+	res = s.data[start:end]
+	return
+}
